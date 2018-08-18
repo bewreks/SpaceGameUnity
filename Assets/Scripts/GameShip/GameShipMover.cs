c93103b676851ca3c3d4d01e86b8e8d09212100d
Дмитрий Соколовский <bewreks@mail.ru>
2018-08-19 00:27:23 +0300

    Change mouse control Add effects for moving

--- Assets/Scripts/GameShip/GameShipMover.cs
+++ Assets/Scripts/GameShip/GameShipMover.cs
@@ -11,8 +11,6 @@ public class GameShipMover : MonoBehaviour
 
     private Camera _camera;
 
-    private Vector3 _prevMousePosition;
-
     private void Awake()
     {
         _camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
@@ -29,27 +27,40 @@ public class GameShipMover : MonoBehaviour
 
     private void Start()
     {
-        _prevMousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-        _prevMousePosition.x = Mathf.Clamp(_prevMousePosition.x, _screenBounds.min.x, _screenBounds.max.x);
-        _prevMousePosition.y = Mathf.Clamp(_prevMousePosition.y, _screenBounds.min.y, _screenBounds.max.y);
-        _prevMousePosition.z = 0;
-        transform.position = _prevMousePosition;
+        var mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.x = Mathf.Clamp(mousePosition.x, _screenBounds.min.x, _screenBounds.max.x);
+        mousePosition.y = Mathf.Clamp(mousePosition.y, _screenBounds.min.y, _screenBounds.max.y);
+        mousePosition.z = 0;
+        transform.position = mousePosition;
     }
 
     void Update()
     {
         #region Mouse Positon
-            var curMousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-            var mouseDelta = curMousePosition - _prevMousePosition;
-            _prevMousePosition = curMousePosition;
+            var x = Input.GetAxis("Mouse X");
+            var y = Input.GetAxis("Mouse Y");
+            var mouseDelta = new Vector3(x * _speed.x * Time.deltaTime, y * _speed.y * Time.deltaTime);
         #endregion
 
         #region Axis Position
             var h = Input.GetAxis("Horizontal");
             var v = Input.GetAxis("Vertical");
-//            _flame.shape.angle = Mathf.Clamp(h, 0, 60);
             var axisDelta = new Vector3(h * _speed.x * Time.deltaTime, v * _speed.y * Time.deltaTime);
         #endregion
+
+        var shapeModule = _flame.shape;
+        var t = mouseDelta.x + axisDelta.x;
+        if (t < 0)
+        {
+            t = -1;
+        }
+        else
+        {
+            t = t > 0 ? 1 : 0;
+        }
+        var shapeModuleAngle = Mathf.LerpUnclamped(30, 0, t);
+        Debug.Log($"{t} {shapeModuleAngle}");
+        shapeModule.angle = shapeModuleAngle;
         
         var position = transform.position + mouseDelta + axisDelta;
         position.x = Mathf.Clamp(position.x, _screenBounds.min.x, _screenBounds.max.x);
--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -12,22 +12,5 @@ public class Shop : MonoBehaviour
 			current = this;
 		}
 	}
-
-	public bool BuyItem(Item item)
-	{
-		return false;
-	}
-}
-
-public class Item
-{
-	private int _cost;
-	private string _name;
-	private string _description;
-
-	public int Cost => _cost;
-	public string Name => _name;
-	public string Description => _description;
-	
 }
 
